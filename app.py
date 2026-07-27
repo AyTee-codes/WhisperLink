789a47a8f5b0db854aff4ca0757f68e1231187a1
AyTee-codes <arneshkomden@gmail.com>
2026-07-27 18:44:07 +0545

    New changes

--- app.py
+++ app.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+
+@app.route("/create")
+def create():
+    return render_template("create.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
